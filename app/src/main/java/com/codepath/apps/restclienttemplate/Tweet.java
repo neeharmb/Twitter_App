645fa719757332f1e256f50c8de2d04232855df7
Neehar Banerjee <neeharmb@fb.com>
2017-06-29 15:22:42 -0700

    made Tweet button prettier

--- app/src/main/java/com/codepath/apps/restclienttemplate/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/Tweet.java
@@ -1,14 +1,8 @@
 package com.codepath.apps.restclienttemplate;
 
-import android.text.format.DateUtils;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.parceler.Parcel;
-
-import java.text.*;
-
-import java.util.Locale;
 
 /**
  * Created by neeharmb on 6/26/17.
@@ -35,26 +29,7 @@ public class Tweet {
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
         tweet.name = "@" + tweet.user.screenName;
-      //  tweet.timeStamp = getRelativeTimeAgo(tweet.createdAt);
         tweet.timeStamp = " · " + TimeFormatter.getTimeDifference(tweet.createdAt);
         return tweet;
-    }
-
-
-    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
-    public static String getRelativeTimeAgo(String rawJsonDate) {
-        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
-        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
-        sf.setLenient(true);
-
-        String relativeDate = "";
-        try {
-            long dateMillis = sf.parse(rawJsonDate).getTime();
-            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return relativeDate;
     }
 }
